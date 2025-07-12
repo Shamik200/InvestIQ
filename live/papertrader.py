056731abd68665b2cd6c92e9f14ab64b12d07338
Shamik200 <119531911+Shamik200@users.noreply.github.com>
2025-07-12 15:45:40 +0530

    Final Commit

--- live/papertrader.py
+++ live/papertrader.py
@@ -38,7 +38,7 @@ FEATURES = [
     'volatility', 'momentum_3'
 ]
 
-exchange = ccxt.coinbasepro()
+exchange = ccxt.coinbase()
 
 def fetch_ohlcv(symbol: str, timeframe:str):
     df = exchange.fetch_ohlcv(symbol, timeframe, limit=100)
